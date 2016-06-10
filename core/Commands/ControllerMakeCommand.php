8d8b22c2f993069cc08ecfe22ec0f931e68db7a2
Lim Kok Suan <admin@gidcs.net>
2016-06-10 19:51:31 +0800

    finish make:controller

---
+++ core/Commands/ControllerMakeCommand.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * KSMVC version 0.1.0
+ * https://github.com/gidcs/ksmvc
+ * Lim Kok Suan <admin@gidcs.net>
+ */
+
+namespace Core\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ControllerMakeCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('make:controller')
+            ->setDescription('Create a new controller class.')
+            ->addArgument(
+                'name',
+                InputArgument::REQUIRED,
+                'What is your controller name?'
+            )
+            ->addOption(
+                'blank',
+                null,
+                InputOption::VALUE_NONE,
+                'If set, the new controller class will be blank.'
+            );
+    }
+
+	protected function getErrorString($error_header='',$error_msg=[]){
+			$maxlen = max(array_map('strlen', $error_msg));
+			$maxlen = max($maxlen,strlen($error_header));
+			$text = '<error>'."\n";
+			$text .= '  '.str_repeat(' ', $maxlen)."  \n";
+			$text .= '  '.$error_header.str_repeat(' ', $maxlen-strlen($error_header))."  \n";
+			foreach($error_msg as $msg){
+				$text .= '  '.$msg.str_repeat(' ', $maxlen-strlen($msg))."  \n";
+			}
+			$text .= '  '.str_repeat(' ', $maxlen)."  ";
+			$text .= '</error>';
+			return $text;
+	}
+	
+	protected function copy_file($source, $dest, $name){
+		$file = file_get_contents($source);
+		$file = str_replace("DummyClass", $name, $file);
+		return file_put_contents($dest, $file, LOCK_EX);
+	}
+	
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $name = $input->getArgument('name');
+		$source = './core/Commands/stub/controller.stub';
+		if ($input->getOption('blank')) {
+            $source .= '.plain';
+        }
+		$dest = './app/controllers/'.$name.'.php';
+		if(file_exists($dest)){
+			$header = '[InvalidArgumentException]';
+			$msg[] = 'The controller class "'.$name.'" already exists';
+			$text=$this->getErrorString($header,$msg);
+		}
+		else{
+			if(!file_exists($source)){
+				$header = '[MissingFileException]';
+				$msg[] = 'Failed to write file to app/controllers/'.$name.'.php';
+				$text=$this->getErrorString($header,$msg);
+			}
+			else if (!$this->copy_file($source,$dest,$name)){
+				$header = '[CopyFailedException]';
+				$msg[] = 'Failed to write file to app/controllers/'.$name.'.php  ';
+				$text=$this->getErrorString($header,$msg);
+			}
+			else{
+				$text = '<info>created</info> app/controllers/'.$name.'.php';
+			}
+		}
+        $output->writeln($text);
+    }
+}
