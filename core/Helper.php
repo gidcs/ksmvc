b7543386f91ff7490810e5e98d8591c3479f78b7
Lim Kok Suan <admin@gidcs.net>
2016-06-18 23:58:37 +0800

    provide @extend for view

--- core/Helper.php
+++ core/Helper.php
@@ -7,6 +7,13 @@ function file_checks($file, $error=true){
 	return 1;
 }
 
+function file_get_contents_checks($file, $error=true){
+	if(!file_exists($file)){
+		if($error) die("Error: File ($file) does not exists.");
+	}
+	return file_get_contents($file);
+}
+
 function method_checks($controller, $method, $error=true){
 	if(!method_exists($controller,$method)){
 		if($error) die("Error: Method ($method) does not exists.");
