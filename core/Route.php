aca6565313f8ce7d555bc35203ea3065d3fa1174
LKS <admin@gidcs.net>
2016-06-20 20:51:43 +0000

    Fix bug of DELETE method

--- core/Route.php
+++ core/Route.php
@@ -103,7 +103,7 @@ class Route{
 			}
 			//get parameter
 			{
-				if(preg_match('/\b(GET)\b/', $req_method)==0){
+				if(preg_match('/\b(GET|DELETE)\b/', $req_method)==0){
 					$param['post_params'] = $_POST;
 				}
 				if(count($matches)>1){
@@ -144,3 +144,4 @@ class Route{
 		self::delete($uri.'/:id',$name.'#destroy');
 	}
 }
+
