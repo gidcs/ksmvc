f8c5a1a58c4aced73e43320ecb10e6f96ea85613
LKS <admin@gidcs.net>
2016-09-24 01:54:00 +0800

    add view files creation

--- core/Commands/ControllerMakeCommand.php
+++ core/Commands/ControllerMakeCommand.php
@@ -48,15 +48,26 @@ class ControllerMakeCommand extends Command
 			return $text;
 	}
 	
-	protected function copy_file($source, $dest, $name){
+	protected function copy_controller_file($source, $dest, $name, $view_dir_name){
 		$file = file_get_contents($source);
 		$file = str_replace("DummyClass", $name, $file);
+		$file = str_replace("Dummy", $view_dir_name, $file);
+		return file_put_contents($dest, $file, LOCK_EX);
+	}
+
+	protected function copy_view_file($source, $dest){
+		$file = file_get_contents($source);
 		return file_put_contents($dest, $file, LOCK_EX);
 	}
 	
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $name = ucfirst($input->getArgument('name')).'Controller';
+        //for view files creation
+		$view_dir_name = lcfirst($input->getArgument('name'));
+		$view_dir = './app/views/'.$view_dir_name;
+		
+		//for controller file creation
+		$name = ucfirst($input->getArgument('name')).'Controller';
 		$source = './core/Commands/stub/controller.stub';
 		if ($input->getOption('blank')) {
             $source .= '.plain';
@@ -66,22 +77,59 @@ class ControllerMakeCommand extends Command
 			$header = '[InvalidArgumentException]';
 			$msg[] = 'The controller class "'.$name.'" already exists';
 			$text=$this->getErrorString($header,$msg);
+			$output->writeln($text);
 		}
 		else{
+			//generate controller file
 			if(!file_exists($source)){
 				$header = '[MissingFileException]';
 				$msg[] = 'Failed to write file to app/controllers/'.$name.'.php';
 				$text=$this->getErrorString($header,$msg);
+				$output->writeln($text);
 			}
-			else if (!$this->copy_file($source,$dest,$name)){
+			else if (!$this->copy_controller_file($source,$dest,$name,$view_dir_name)){
 				$header = '[CopyFailedException]';
 				$msg[] = 'Failed to write file to app/controllers/'.$name.'.php  ';
 				$text=$this->getErrorString($header,$msg);
+				$output->writeln($text);
 			}
 			else{
 				$text = '<info>created</info> app/controllers/'.$name.'.php';
+				$output->writeln($text);
+				//generate view files
+				if ($input->getOption('blank')) {
+					//no view files needed
+				}
+				else{
+					//create directory
+					if(!mkdir($view_dir, 0755)){
+						$header = '[MkdirFailedException]';
+						$msg[] = 'Failed to create directory app/views/'.$view_dir_name;
+						$text=$this->getErrorString($header,$msg);
+						$output->writeln($text);
+					}
+					else{
+						$text = '<info>directory created</info> app/views/'.$view_dir_name;
+						$output->writeln($text);
+						//copy view file
+						$views = ['index', 'create', 'show', 'edit'];
+						$source = './core/Commands/stub/view.stub';
+						foreach($views as $v){
+							$dest = $view_dir.'/'.$v.'.php';
+							if(!$this->copy_view_file($source,$dest)){	
+								$header = '[CopyFailedException]';
+								$msg[] = 'Failed to write file to app/views/'.$view_dir_name.'/'.$v.'.php  ';
+								$text=$this->getErrorString($header,$msg);
+								$output->writeln($text);
+							}
+							else{
+								$text = '<info>created</info> app/views/'.$view_dir_name.'/'.$v.'.php';
+								$output->writeln($text);
+							}
+						}
+					}
+				}
 			}
 		}
-        $output->writeln($text);
     }
 }
