3fd5a2eca34f9ba092e8442f42b012bfb5187fa6
LKS <admin@gidcs.net>
2016-09-24 11:02:58 +0800

    modify getSettings.php

--- app/views/auth/getSettings.php
+++ app/views/auth/getSettings.php
@@ -7,7 +7,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Settings</div>
+                <div class="panel-heading">Profile</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="/profile_settings">
                         <div class="form-group">
