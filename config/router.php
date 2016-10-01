18ee7b91eca6ccd015fbc201ee91353e63eae4cd
LKS <admin@gidcs.net>
2016-10-02 00:50:36 +0800

    remove newline in config/router

--- config/router.php
+++ config/router.php
@@ -5,7 +5,7 @@ Route::Auth();
 Route::all('/manage/users', 'UsersController', [
   'except' => ['show']
 ]);
-
 Route::get('/manage/settings','OptionsController#edit');
 Route::put('/manage/settings/:name','OptionsController#update');
+
 //Route::print_route();
